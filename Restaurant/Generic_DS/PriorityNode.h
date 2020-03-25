414015ecf60d19bc25f826d63edb4685b3b4cdf2
donia.meselhi01@eng-st.cu.edu.eg <donia.meselhi01@eng-st.cu.edu.eg>
2020-03-25 16:21:46 +0200

    priorit

---
+++ Restaurant/Generic_DS/PriorityNode.h
@@ -0,0 +1,81 @@
+
+#ifndef __NODE_H_
+#define __NODE_H_
+
+template < typename T>
+class PriorityNode
+{
+private:
+	T item;// A data item
+
+	int priority;//priority of data to insert in the queue
+	PriorityNode<T>* next; // Pointer to next node
+public:
+	PriorityNode();
+	PriorityNode(const T& r_Item);	//passing by const ref.
+	PriorityNode(const T& r_Item, PriorityNode* nextNodePtr);
+	void setItem(const T& r_Item);
+	void setNext(PriorityNode<T>* nextNodePtr);
+	void setPriority(const int& priority);
+	int GetPriority()const;
+	T getItem() const;
+	PriorityNode<T>* getNext() const;
+}; // end p. Node
+
+
+template < typename T>
+PriorityNode<T>::PriorityNode()
+{
+	next = nullptr;
+	priority = 0;
+}
+
+template < typename T>
+PriorityNode<T>::PriorityNode(const T& r_Item,const int& pri)
+{
+	priority = pri;
+	item = r_Item;
+	next = nullptr;
+}
+
+template < typename T>
+PriorityNode<T>::PriorityNode(const T& r_Item, const int& pri, Node<T>* nextNodePtr)
+{
+	setPriority(pri);
+	item = r_Item;
+	next = nextNodePtr;
+}
+template < typename T>
+void PriorityNode<T>::setItem(const T& r_Item)
+{
+	item = r_Item;
+}
+template < typename T>
+void PriorityNode<T>::setPriority(const int& pri)
+{
+	if(pri>=0)
+		priority = pri;
+}
+template < typename T>
+void PriorityNode<T>::setNext(Node<T>* nextNodePtr)
+{
+	next = nextNodePtr;
+}
+
+template < typename T>
+T PriorityNode<T>::getItem() const
+{
+	return item;
+}
+template < typename T>
+T PriorityNode<T>::GetPriority() const
+{
+	return priority;
+}
+template < typename T>
+PriorityNode<T>* PriorityNode<T>::getNext() const
+{
+	return next;
+}
+
+#endif#pragma once
