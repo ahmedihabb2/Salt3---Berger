0c33900391cc2215a7bb682f4fded7d25e7e36e6
donia.meselhi01@eng-st.cu.edu.eg <donia.meselhi01@eng-st.cu.edu.eg>
2020-03-27 16:48:59 +0200

    Donia First work (mostly not finished yet ... I updated both files Order.h&&Order.cpp ... kindly ,Check those changes)

--- Restaurant/Rest/Order.cpp
+++ Restaurant/Rest/Order.cpp
@@ -1,10 +1,13 @@
 #include "Order.h"
 
-Order::Order(int id, ORD_TYPE r_Type)
+Order::Order(int id, ORD_TYPE r_Type,const int&totalmoney)
 {
 	ID = (id>0&&id<1000)?id:0;	//1<ID<999
 	type = r_Type;
 	status = WAIT;
+	WaitTime = 0;//initially 0 and will increment each time step
+	totalMoney = totalmoney;
+	
 }
 
 Order::~Order()
@@ -44,3 +47,77 @@ ORD_STATUS Order::getStatus() const
 	return status;
 }
 
+//////////////////////Donia Additions
+void Order::setOrderSize(int size)
+{
+	orderSize = size;
+}
+
+void Order::setPriority()
+{
+	Priority = float(totalMoney) / (ArrTime * orderSize);
+
+}
+int Order::getOrderSize()
+{
+	return orderSize;
+}
+int Order::getArrTime()
+{
+	return ArrTime;
+}
+int Order::getServTime()
+{
+	return ServTime;
+}
+int Order::getServInt()
+{
+	return srvInt;
+}
+int Order::getWaitTime()
+{
+	return WaitTime;
+}
+int Order::getFinishTime()
+{
+	return FinishTime;
+}
+
+float Order::getPriority()
+{
+	return  Priority;
+}
+void Order::Promote(Order& promoted,int& addedmoney)
+{
+	promoted.type = TYPE_VIP;
+	promoted.totalMoney += addedmoney;
+}
+void Order::setArrTime(const int& arr)
+{
+	ArrTime = arr;
+}
+
+void  Order::setServTime(const int& serv)///as TimeStep
+{
+	ServTime = serv;
+}
+void  Order::setServInt(const int& serv)////as time interval
+{
+	srvInt = serv;/////will depend on the cook
+}
+void Order::setWaitTime(const int& Wait)
+{
+	WaitTime = ServTime -ArrTime;
+}
+void Order::setFinishTime()
+{
+	FinishTime = ArrTime + srvInt + WaitTime;
+}
+void Order::Serve(const int& ArrT, const int& servInt, const int& Size)
+{
+	setArrTime(ArrT);
+	
+	setFinishTime();
+
+
+}
--- Restaurant/Rest/Order.h
+++ Restaurant/Rest/Order.h
@@ -11,21 +11,25 @@ protected:
 	ORD_TYPE type;		//order type: Normal, vegan, VIP
 	ORD_STATUS status;	//waiting, in-service, done
 	int Distance;	//The distance (in meters) between the order location and the resturant 
-	                
+
 	double totalMoney;	//Total order money
 
 	int ArrTime, ServTime, FinishTime;	//arrival, service start, and finish times
-	
-	
+
 	//
 	// TODO: Add More Data Members As Needed
 	//
+	int orderSize;
+	float Priority;
+	int WaitTime;
+	int srvInt;
 
 public:
-	Order(int ID, ORD_TYPE r_Type);
+	Order(int ID, ORD_TYPE r_Type,  const int& totalmoney);
 	virtual ~Order();
 
 	int GetID();
+
 
 	ORD_TYPE GetType() const;
 
@@ -34,11 +38,27 @@ public:
 
 	void setStatus(ORD_STATUS s);
 	ORD_STATUS getStatus() const;
-	
+
 	//
 	// TODO: Add More Member Functions As Needed
-	//
+	//Donia Addition
+	void setPriority();
+	void setOrderSize(int size);
+	float getPriority();
+	int getOrderSize();
+	int getArrTime();
+	int getServTime();
+	int getServInt();
+	int getWaitTime();
+	int getFinishTime();
+	void Promote(Order& promoted, int& addedmoney);
+	void setArrTime(const int& arr);
 
+	void setServTime(const int& serv);
+	void setServInt(const int& serv);
+	void setFinishTime();
+	void setWaitTime(const int&);
+	void Serve(const int& ArrT, const int& Servtime, const int& Size);
 };
 
 #endif
