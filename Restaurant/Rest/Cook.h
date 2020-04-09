2c9089b5292b660453df9ce83cf6608888714340
raghoda1234692000@gmail.com <raghoda1234692000@gmail.com>
2020-04-09 16:45:02 +0300

    again

--- Restaurant/Rest/Cook.h
+++ Restaurant/Rest/Cook.h
@@ -9,14 +9,30 @@ class Cook
 	ORD_TYPE type;	//for each order type there is a corresponding type (VIP, Normal, Vegan)
 	int speed;		//dishes it can prepare in one clock tick (in one timestep)
 
+	//abeer
+	int numOrdersBefBreak; 
+	int breakDuration; 
+
 public:
 	Cook();
 	virtual ~Cook();
+
 	int GetID() const;
 	ORD_TYPE GetType() const;
+
+	//abeer
+	int getSpeed() const;
+	int getNumOrdBbreak() const;
+	int getBreakDur() const;
+
+	//////////////////////////////////////////////////
+
 	void setID(int);
 	void setType(ORD_TYPE) ;
 
-	void setSpeed(int); //abeer added this
+	//abeer
+	void setSpeed(int); 
+	void setNumOrdBbreak(int num);
+	void setBreakDur(int bd);
 
 };
