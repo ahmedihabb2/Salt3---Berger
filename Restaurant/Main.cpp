1c8107df60d61df7b1e1dcdc409d6ff2b607b65a
Ahmed Ihab <ahmedbaz20@hotmail.com>
2020-05-23 03:59:15 +0200

    Spong

--- Restaurant/Main.cpp
+++ Restaurant/Main.cpp
@@ -1,11 +1,14 @@
 //#include "Drawing.h"
 #include "Rest\Restaurant.h"
 #include "GUI\GUI.h"
+#include<Windows.h>
+#include<mmsystem.h>
 
 int main()
 {
 	Restaurant* pRest = new Restaurant;
 	pRest->RunSimulation();
+	PlaySound(TEXT("Spong.wav"), NULL, SND_SYNC);
 	delete pRest;
 	return 0;
 }
